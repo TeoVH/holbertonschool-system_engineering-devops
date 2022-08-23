f8594c3902381c1d7ec16a1dff9ebc4416d0656f
TeoVH <villadamateo64@gmail.com>
2022-08-23 17:15:18 -0500

    Using puppet set up your SSH configuration to connect to a server without typing a password

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# Using puppet set up your SSH configuration to connect to a server without typing a password
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+}
